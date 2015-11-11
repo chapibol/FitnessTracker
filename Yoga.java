0df156bb6aae016600bcfd395bd2ccf48f8a31f4
ddesouz2 <ddesouz2@users.noreply.github.com>
2015-11-11 16:24:23 -0500

    Update Yoga.java

--- Yoga.java
+++ Yoga.java
@@ -9,16 +9,32 @@
  * @author Daniel
  */
 public class Yoga extends ExerciseActivity{
+    private int duration;
+    private double weight;
     
     public Yoga(){
         
     }
+    public Yoga(int duration, double weight){
+      this.duration = duration;
+      this.weight = weight;
+    }   
+    
+    public int getDuration(){
+      return duration;
+    }
+    public void setDuration(int duration) throws InvalidInputException{
+      if(duration <= 0 || duration > 999){
+         throw new InvalidInputException("Error: Invalid entry.");
+      }
+      this.duration = duration;
+    }
+        
+    public double calculateCaloriesBurned(){
+         return 0.0;
+    }
     
     public String toString(){
     	return "Duration: " + super.getDuration();
-    }
-    
-    public double calculateCaloriesBurned(){
-         return 0.0;
     }
 }
