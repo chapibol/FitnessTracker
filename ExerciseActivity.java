26cf953374afe5d21be0840ef8cc96c05a26596e
ddesouz2 <ddesouz2@users.noreply.github.com>
2015-11-11 16:17:39 -0500

    Update ExerciseActivity.java

--- ExerciseActivity.java
+++ ExerciseActivity.java
@@ -12,7 +12,6 @@
 import java.util.Date;
 public abstract class ExerciseActivity {
     private int userId;
-    private int duration;
     private Date date;
     private double caloriesBurned;
     
@@ -20,18 +19,14 @@ public abstract class ExerciseActivity {
         
     }
     
-    public ExerciseActivity(int userId, int duration, Date date){
+    public ExerciseActivity(int userId, Date date){
         this.userId = userId;
-        this.duration = duration;
         this.date = date;
     }
     
     //Accessors below
     public int getUserId(){
         return userId;
-    }
-    public int getDuration(){
-        return duration;
     }
     public Date getDate(){
         return date;
@@ -40,12 +35,6 @@ public abstract class ExerciseActivity {
     //Mutators below
     public void setUserId(int id){
         userId = id;
-    }
-    public void setDuration(int duration) throws InvalidInputException{
-      if(duration <= 0 || duration > 999){
-         throw new InvalidInputException("Error: Invalid entry.");
-      }
-      this.duration = duration;
     }
     public void setDate(Date date){
         this.date = date;
