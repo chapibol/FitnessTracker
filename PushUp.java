bd11f4bf6a5c8543be251af7aeaa1a1c08bf0ea8
ddesouz2 <ddesouz2@users.noreply.github.com>
2015-11-11 16:23:21 -0500

    Update PushUp.java

--- PushUp.java
+++ PushUp.java
@@ -1,7 +1,5 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, cho/*
- * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -41,41 +39,6 @@ public class PushUp extends ExerciseActivity{
     
     public String toString(){
         return "";
-    }
-    
-}
-ose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author Daniel
- */
-public class PushUp extends ExerciseActivity{
-   private int quantity;
-   
-    public PushUp(){
-        
-    }
-    public PushUp(int quantity){
-         this.quantity = quantity;
-    }
-    
-    public int getQuantity(){
-      return quantity;
-   }
-    
-    public void setQuantity(int quantity) throws InvalidInputException{
-      if(quantity < 0 || quantity > 999){
-         throw new InvalidInputException("Error: Entry should be between 0 and 999");
-      }
-      this.quantity = quantity;
-   }
-   
-    public double calculateCaloriesBurned(){
-         return 0.0;
     }
     
 }
