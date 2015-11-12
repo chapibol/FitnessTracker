ee3c598830dff6c86f7962b90a3182404fdda207
ddesouz2 <ddesouz2@users.noreply.github.com>
2015-11-11 23:25:25 -0500

    Update InvalidInputException.java

--- InvalidInputException.java
+++ InvalidInputException.java
@@ -1,4 +1,11 @@
-
+/*
+ * Class InvalidInputException
+ *
+ * This class provides an exception for invalid input.
+ * 
+ * @author Daniel de Souza, Luis Velasco
+ */
+ 
 public class InvalidInputException extends Exception {
 
 	public InvalidInputException(){
